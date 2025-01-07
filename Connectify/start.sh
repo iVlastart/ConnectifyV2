d623fd16df1d1295cd206159ad9616e2469820aa
VlastimilVasut <vasutvlastimil@gmail.com>
2025-01-07 19:14:18 +0100

    `Update start.sh script with minor changes to echo messages`

--- Connectify/start.sh
+++ Connectify/start.sh
@@ -1,9 +1,9 @@
 #cmd /k start.sh
 
-echo "Starting PHP dev server"
+echo "Starting PHP dev server..."
 php artisan serve &
 
-echo "Starting NPM dev server"
+echo "Starting NPM dev server..."
 npm run dev &
 
 echo "THIS IS THE WEB APP LOCAL URL => http://127.0.0.1:8000"
